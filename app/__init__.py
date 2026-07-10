e7bee3cd91e788c792367ba28c726204bbe0ed46
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-07-10 21:18:03 +0300

    Add service listing and details templates, and register service blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template
 from app.user.api.user_routes import user_bp
-
+from app.service.api.service_routes import service_bp
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
-    
+    app.config.from_object("config.Config")
+
     register_blueprints(app)
     register_pages(app)
-    
+
     return app
 
 
 def register_blueprints(app):
     app.register_blueprint(user_bp)
+    app.register_blueprint(service_bp)
 
 
 def register_pages(app):
-    
     @app.get("/")
     def home():
         return render_template("home.html")
