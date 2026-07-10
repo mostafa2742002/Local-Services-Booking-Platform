4db233e0ee0d201ea8bb86ac69b6c1ecf1788873
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-07-10 15:58:05 +0300

    Refactor app initialization to register blueprints and update home route message

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from app.user.api.user_routes import user_bp
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
     
-    @app.route('/')
-    def home():
-        return "Local Services"
+    register_blueprints(app)
+    register_pages(app)
     
     return app
+
+
+def register_blueprints(app):
+    app.register_blueprint(user_bp)
+
+
+def register_pages(app):
+    
+    @app.get("/")
+    def home():
+        return "Local Services Booking Platform is running"
